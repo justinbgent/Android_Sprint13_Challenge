ce4e2fdc19d2b83b5425b4e58cebb4b9d83be61a
Justin Gent <justinbgent@gmail.com>
2019-11-01 10:12:31 -0600

    Build retrofit functions and api to get makeup products by brand

--- app/src/main/java/com/schoolwork/makeupapi/retrofit/Retrofit.kt
+++ app/src/main/java/com/schoolwork/makeupapi/retrofit/Retrofit.kt
@@ -1,12 +1,34 @@
 package com.schoolwork.makeupapi.retrofit
 
-data class MakeupProduct(val brand: String)
+import com.google.gson.GsonBuilder
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+data class MakeupProduct(val name: String, val price: String, val image_link: String, val rating: String?)
 
 
 interface MakeupApi{
-
+    @GET("products.json")
+    fun getProductList(@Query("brand")brand: String): Single<Array<MakeupProduct>>
 }
 
 object Retrofit {
+    const val BASE_URL = "https://makeup-api.herokuapp.com/api/v1/"
+
+    fun getProducts(brand: String): Single<Array<MakeupProduct>>{
+        val builder = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(MakeupApi::class.java)
+
+        return builder.getProductList(brand)
+    }
+
 
 }
